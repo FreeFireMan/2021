ba2c71682d83d040078786c6d8f32c45df23f699
RingiBoy <mitya.info@gmail.com>
2021-04-30 12:29:49 +0300

    1111

---
+++ React/lesson-1/src/App.js
@@ -0,0 +1,23 @@
+import Car from "./components/Car";
+
+
+function App() {
+  return (
+    <div>
+      <Car
+       model={'bmw'}
+       description={'lordfg sfsdfsdf'}
+       power={'4'}
+        />
+     
+     <Car
+       model={'slavuta'}
+       description={'gavno mashina'}
+       power={'0,1'}
+        />
+
+    </div>
+  );
+}
+
+export default App;
