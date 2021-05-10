98c6ea3c4921daee426461e085df61737849e9c0
ringi <mitya.info@gmail.com>
2021-05-10 23:34:24 +0300

    lesson3part1

--- react/hw/myapp/src/components/users/Users.js
+++ react/hw/myapp/src/components/users/Users.js
@@ -1,17 +1,41 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import User from '../user/User';
+import '../users/Users.css';
+
 export default function Users() {
     
     const [users, setUsers] = useState([]);
+    const [singleUser, setSingleUser] = useState()
 
-    fetch('https://jsonplaceholder.typicode.com/users')
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
     .then(value=> value.json())
     .then(value=> {
         setUsers([...value]);
-    });
+        });
+    },[]);
     
+const search = (id)=>{
+    let findedUser = users.find(value=>value.id===id);
+    console.log(findedUser);
+    setSingleUser(findedUser)
+};
+
     return (
-        <div>
-            {users.map(value=> <div>{value.name}</div>)}
+        <div className={'wrap'}>
+
+            <div className={'users-box'}>
+            {users.map(value=> <User key={value.id} item={value} search={search}/>)}
+            </div>
+        
+            <div className={'user-description-box'}>
+            { singleUser&& <h2>{singleUser.id}-{singleUser.username}</h2>
+
+
+            }
+
+            </div>
+
         </div>
     )
 }
